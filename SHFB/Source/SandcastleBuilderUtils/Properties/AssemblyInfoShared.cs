6bf123dc06a87a014dfd38300c93887755c71bac
Eric Woodruff <Eric@EWoodruff.us>
2021-11-07 10:59:11 -0800

    Minor fix to HTML extract tool (#876)

--- SHFB/Source/SandcastleBuilderUtils/Properties/AssemblyInfoShared.cs
+++ SHFB/Source/SandcastleBuilderUtils/Properties/AssemblyInfoShared.cs
@@ -2,7 +2,7 @@
 // System  : Sandcastle Help File Builder
 // File    : AssemblyInfoShared.cs
 // Author  : Eric Woodruff  (Eric@EWoodruff.us)
-// Updated : 10/23/2021
+// Updated : 11/07/2021
 // Note    : Copyright 2006-2021, Eric Woodruff, All rights reserved
 //
 // Sandcastle Help File Builder common assembly attributes.
@@ -90,13 +90,13 @@ internal static partial class AssemblyInfo
     //
     // This is used to set the assembly file version.  This will change with each new release.  MSIs only
     // support a Major value between 0 and 255 so we drop the century from the year on this one.
-    public const string FileVersion = "21.10.23.0";
+    public const string FileVersion = "21.11.7.0";
 
     // Common product version
     //
     // This may contain additional text to indicate Alpha or Beta states.  The version number will always match
     // the file version above but includes the century on the year.
-    public const string ProductVersion = "2021.10.23.0";
+    public const string ProductVersion = "2021.11.7.0";
 
     // Assembly copyright information
     public const string Copyright = "Copyright \xA9 2006-2021, Eric Woodruff, All Rights Reserved";
