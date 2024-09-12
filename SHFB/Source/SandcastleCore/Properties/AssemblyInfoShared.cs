f88e8feb48b2a27566a0bd36ff97bf09d9252b3b
EWSoftware <Eric@EWoodruff.us>
2024-09-12 11:04:31 -0700

    Updates for publication

--- SHFB/Source/SandcastleCore/Properties/AssemblyInfoShared.cs
+++ SHFB/Source/SandcastleCore/Properties/AssemblyInfoShared.cs
@@ -1,7 +1,7 @@
 //===============================================================================================================
 // System  : Sandcastle Tools
 // File    : AssemblyInfoShared.cs
-// Updated : 02/18/2024
+// Updated : 09/12/2024
 // Note    : Copyright 2006-2024, Microsoft Corporation, All rights reserved
 //
 // Sandcastle tools common assembly attributes.
@@ -73,13 +73,13 @@ internal static partial class AssemblyInfo
     //
     // This is used to set the assembly file version.  This will change with each new release.  MSIs only
     // support a Major value between 0 and 255 so we drop the century from the year on this one.
-    public const string FileVersion = "24.2.18.0";
+    public const string FileVersion = "24.9.12.0";
 
     // Common product version
     //
     // This may contain additional text to indicate Alpha or Beta states.  The version number will always match
     // the file version above but includes the century on the year.
-    public const string ProductVersion = "2024.2.18.0";
+    public const string ProductVersion = "2024.9.12.0";
 
     // Assembly copyright information
     public const string Copyright = "Copyright \xA9 2006-2024, Microsoft Corporation, All Rights Reserved.\r\n" +
